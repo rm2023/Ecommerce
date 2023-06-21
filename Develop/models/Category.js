03fbbf1e9b44bd2aaddcdcf5948529384d5f8184
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-06-21 15:04:19 -0400

    fixed route errors

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,78 +1,30 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Model, DataTypes } = require('sequelize');
 
-// The `/api/categories` endpoint
+const sequelize = require('../config/connection.js');
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  Category.findAll({
-    include: [Product],
-  })
-    .then((dbRes) => {
-      res.json(dbRes);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+class Category extends Model {}
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  Category.findOne({
-    where: {
-      id: req.params.id,
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    include: [Product],
-  })
-    .then((dbRes) => {
-      res.json(dbRes);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-  Category.create(req.body)
-  .then((dbRes) => {
-    res.json(dbRes);
-  })
-  .catch((err) => {
-    res.json(err);
-  });
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-  })
-    .then((dbRes) => {
-      res.json(dbRes);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  Category.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbRes) => {
-      res.json(dbRes);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-module.exports = router;
+module.exports = Category;
